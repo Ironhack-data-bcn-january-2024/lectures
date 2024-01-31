e3761e692ba0a79d1aac8e62950be7b55b7c9692
breogann <fernandocostapradillo@gmail.com>
2024-01-31 13:02:04 +0100

    uploading

--- week-3/01. MySQL scripts/2. select-employees.sql
+++ week-3/01. MySQL scripts/2. select-employees.sql
@@ -182,3 +182,218 @@ SELECT
 	-- 1. Table
     -- 2. Columns
     -- 3. Keys we're joining by
+    
+SELECT DISTINCT(first_name) FROM employees;
+
+SELECT DISTINCT(CONCAT (first_name, " ", last_name) ) AS unique_names
+FROM employees;
+  
+SELECT COUNT(DISTINCT(CONCAT (first_name, " ", last_name))) AS unique_names
+FROM employees; -- 27k
+
+-- Whaty is the average salary by sex
+	-- grouping by: gender
+    -- aggregate: avg(salary)
+    -- gender & salary
+    -- salaries:  salary
+    -- employees: gender
+    
+SELECT *,
+	gender, -- Table 1: employees
+    salary -- Table 2: salaries
+FROM
+	employees 
+    JOIN salaries
+		ON employees.emp_no = salaries.emp_no; -- 3. Common key: emp_no
+    
+    
+SELECT
+	gender, -- Table 1: employees
+    -- salary, -- Table 2: salaries
+    AVG(salary)
+FROM
+	employees 
+    JOIN salaries
+		ON employees.emp_no = salaries.emp_no
+GROUP BY (gender);
+
+    
+SELECT
+	gender, -- Table 1: employees
+    AVG(salary)
+FROM
+	employees 
+    JOIN salaries
+		ON employees.emp_no = salaries.emp_no
+GROUP BY (gender);
+    
+-- Error Code: 1055. Expression #2 of SELECT list is not in GROUP BY clause 
+-- and contains nonaggregated column 'employees.salaries.salary' 
+-- which is not functionally dependent on columns in GROUP BY clause;
+--  this is incompatible with sql_mode=only_full_group_by	0.00040 se
+
+
+-- How many people are M & F
+
+SELECT gender, COUNT(gender)
+FROM employees
+GROUP BY gender;
+
+-- What are the most frequent names
+SELECT first_name, COUNT(first_name) AS the_count
+from employees
+GROUP BY first_name
+ORDER BY the_count DESC;
+     
+-- The MOST repeated
+SELECT first_name, COUNT(first_name) AS the_count
+from employees
+GROUP BY first_name
+ORDER BY the_count desc
+LIMIT 1;
+
+
+SELECT first_name, COUNT(first_name) AS the_count
+from employees
+GROUP BY first_name
+ORDER BY the_count desc;
+
+-- All the people in the company where their names
+-- are repeated more than 270
+-- filter
+
+SELECT first_name, COUNT(first_name) AS the_count
+FROM employees
+	-- filtering: where
+		-- where is a filter that happens BEFORE GROUPING
+GROUP BY first_name
+	HAVING the_count > 290
+ORDER BY the_count DESC;
+
+-- ALl the women whose name happens more than 120 times
+SELECT first_name, COUNT(first_name) AS the_count, gender
+FROM employees
+	WHERE gender = "F"
+GROUP BY first_name
+	HAVING the_count > 120
+ORDER BY the_count DESC;
+
+
+SELECT first_name
+FROM employees
+	WHERE gender = "F"; -- where filters row by row (unnagregated)
+    
+-- HAVING vs. WHERE
+	-- Having is filtering based on the value of the aggregation
+    -- Where: filters before
+    
+
+select * from departments;
+
+-- MANAGERS VERSION
+-- 0. Join employees & departments: who belongs where
+-- 1. How many employees each departmen has: group by
+-- 2. Having: only show me employees belonging to big / small department
+
+-- 0. Join employees & departments: who belongs where
+SELECT
+	first_name, last_name, -- Table 1: employees
+	dept_name -- Table 3: departments
+	
+    FROM
+		employees
+	JOIN
+		dept_manager
+			ON employees.emp_no = dept_manager.emp_no -- Key 1: emp_no
+	JOIN 
+		departments
+			ON dept_manager.dept_no = departments.dept_no; -- Key 2: dept_no
+
+
+SELECT
+	dept_name,
+    COUNT(dept_name)
+	
+    FROM
+		employees
+	JOIN
+		dept_manager
+			ON employees.emp_no = dept_manager.emp_no -- Key 1: emp_no
+	JOIN 
+		departments
+			ON dept_manager.dept_no = departments.dept_no -- Key 2: dept_no
+            
+	GROUP BY dept_name
+    ORDER BY COUNT(dept_name) DESC;
+
+-- EMPLOYEES VERSION
+-- 0. Join employees & departments: who belongs where
+SELECT
+	first_name, last_name, -- Table 1: employees
+	dept_name -- Table 3: departments
+	
+    FROM
+		employees
+	JOIN
+		dept_emp
+			ON employees.emp_no = dept_emp.emp_no -- Key 1: emp_no
+	JOIN 
+		departments
+			ON dept_emp.dept_no = departments.dept_no; -- Key 2: dept_no
+
+-- 1. How many employees each departmen has: group by
+SELECT
+	dept_name,
+    COUNT(dept_name) as the_count
+	
+    FROM
+		employees
+	JOIN
+		dept_emp
+			ON employees.emp_no = dept_emp.emp_no -- Key 1: emp_no
+	JOIN 
+		departments
+			ON dept_emp.dept_no = departments.dept_no -- Key 2: dept_no
+            
+	GROUP BY dept_name
+    ORDER BY the_count DESC;
+
+
+-- 2. Having: only show me employees belonging to big / small department
+SELECT
+	dept_name,
+    COUNT(dept_name) as the_count
+	
+    FROM
+		employees
+	JOIN
+		dept_emp
+			ON employees.emp_no = dept_emp.emp_no -- Key 1: emp_no
+	JOIN 
+		departments
+			ON dept_emp.dept_no = departments.dept_no -- Key 2: dept_no
+	GROUP BY dept_name
+    HAVING the_count > 50000
+    ORDER BY the_count DESC;
+
+-- 2. Having: only show me employees belonging to big / small department
+SELECT
+	dept_name,
+    COUNT(dept_name) as the_count
+	
+    FROM
+		employees
+	JOIN
+		dept_emp
+			ON employees.emp_no = dept_emp.emp_no -- Key 1: emp_no
+	JOIN 
+		departments
+			ON dept_emp.dept_no = departments.dept_no -- Key 2: dept_no
+	WHERE gender = "F" -- With all the rows. Non-aggregated, before grouping by
+	GROUP BY dept_name
+		HAVING the_count > 20000 -- After aggregation is done
+    ORDER BY the_count DESC;
+
+    
+    
+    
